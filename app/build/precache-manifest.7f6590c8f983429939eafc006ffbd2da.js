b0c32194ac192311a0640a26ece507b83e066438
Parker Johnson <mrstormthebeach@gmail.com>
2018-10-14 16:04:23 -0700

    add static build

---
+++ app/build/precache-manifest.7f6590c8f983429939eafc006ffbd2da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "758f1c7f248c38a57646",
+    "url": "/static/js/main.758f1c7f.chunk.js"
+  },
+  {
+    "revision": "b321012661ba637cf63a",
+    "url": "/static/js/1.b3210126.chunk.js"
+  },
+  {
+    "revision": "758f1c7f248c38a57646",
+    "url": "/static/css/main.a5ea2bbc.chunk.css"
+  },
+  {
+    "revision": "20db598b48a566f74408a4f0d31e0ab8",
+    "url": "/index.html"
+  }
+];
