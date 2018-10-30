628aae002a051c1ad0a8cd97f0d5a597f4641d4a
Parker Johnson <mrstormthebeach@gmail.com>
2018-10-29 20:28:11 -0700

    view rankings now shows rankings

--- app/src/ViewPowerRankings.js
+++ app/src/ViewPowerRankings.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
+import Requests from './http/requests'
+import PowerRankingComponent from './PowerRankingComponent'
 
 class ViewPowerRankings extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      rankingsList: null
+    }
   }
 
+  componentDidMount() {
+    Requests.getPowerRankings().then(res => {
+      this.setState({ rankingsList: res })
+    })
+  }
+
+  renderRankings = () => {
+    let rankingsDiv = []
+    if(this.state.rankingsList) {
+      this.state.rankingsList.forEach((ranking) => {
+        rankingsDiv.push(<PowerRankingComponent key={ ranking.teamId } ranking={ranking}/>)
+      })
+    } else {
+      rankingsDiv.push(<p>Imagine these are the power rankings</p>)
+    }
+    return rankingsDiv
+  }
   render() {
     return (
       <div>
-        <p>Imagine these are the power rankings</p>
+        {this.renderRankings()}
       </div>
     )
   }
--- server/src/server.js
+++ server/src/server.js
@@ -1,77 +1,80 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import path from 'path';
-import fs from 'fs';
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import path from 'path'
+import fs from 'fs'
 
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 import UserLogin from './model/UserLogin'
 import PowerRanking from './model/PowerRanking'
 
 import Moment from 'moment'
 import DateUtils from './DateUtils'
 
-let Schema = mongoose.Schema;
+let Schema = mongoose.Schema
 
 const app = express()
-app.use(bodyParser.json({limit: "50mb"}))
-app.use(bodyParser.urlencoded({limit:"50mb", extended:true, paremeterLimit:50000}))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    paremeterLimit: 50000
+  })
+)
 
 let mongoDB = 'mongodb://localhost/test'
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise; // Weird
+mongoose.connect(mongoDB)
+mongoose.Promise = global.Promise // Weird
 let db = mongoose.connection
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 db.once('open', function() {
   console.log('connection open!!')
 })
-
 
 app.use(cors())
 
 app.get('/data/test', (req, res) => {
   res.send({
-    "hello": "world",
-    "name": "parker Johnson",
-    "data": {
-      "data1": "true",
-      "data2": false
+    hello: 'world',
+    name: 'parker Johnson',
+    data: {
+      data1: 'true',
+      data2: false
     }
   })
 })
 
 app.get('/data/:year', (req, res) => {
-    const year = req.params.year
-    const filePath = path.resolve(process.cwd(), 'data/'+year+'.json')
-    fs.stat(filePath, (error) => {
-        if(error) {
-            console.error('we got an error', error)
-            res.send(404)
+  const year = req.params.year
+  const filePath = path.resolve(process.cwd(), 'data/' + year + '.json')
+  fs.stat(filePath, error => {
+    if (error) {
+      console.error('we got an error', error)
+      res.send(404)
+    } else {
+      fs.readFile(filePath, 'utf8', (error, file) => {
+        if (error) {
+          console.error('Error reading file ' + filePath, error)
+          res.send(500)
         } else {
-            fs.readFile(filePath, 'utf8', (error, file) => {
-                if(error) {
-                    console.error('Error reading file '+filePath, error)
-                    res.send(500)
-                } else {
-                    res.send(file)
-                }
-            })
+          res.send(file)
         }
-    })
+      })
+    }
+  })
 })
 
 app.post('/api/login', (req, res) => {
   let userData = {}
-  if(req.body.username &&
-      req.body.password) {
-
+  if (req.body.username && req.body.password) {
     userData = {
-      username: req.body.username,
+      username: req.body.username
     }
   }
 
   UserLogin.find(userData, function(err, user) {
-    if(user[0].password === req.body.password) {
+    if (user[0].password === req.body.password) {
       console.log('user passed auth', user)
 
       let data = {
@@ -79,7 +82,7 @@ app.post('/api/login', (req, res) => {
         username: user[0].username,
         emoji: user[0].emoji
       }
-      
+
       res.status(200).send(data)
     } else {
       console.warn('user failed auth', req.body.username, req.body.password)
@@ -91,36 +94,39 @@ app.post('/api/login', (req, res) => {
 app.get('/api/getUsers', (req, res) => {
   UserLogin.find({}, function(err, users) {
     let data = []
-    users.forEach((user) => {
-      data.push({username: user.username})
+    users.forEach(user => {
+      data.push({ username: user.username })
     })
-    res.status(200).send(data);
+    res.status(200).send(data)
   })
 })
 
 app.get('/api/getTeams', (req, res) => {
   UserLogin.find({}, function(err, users) {
     let data = []
-    users.forEach((user) => {
-      data.push({username: user.username, teamId: user.teamId, emoji: user.emoji})
+    users.forEach(user => {
+      data.push({
+        username: user.username,
+        teamId: user.teamId,
+        emoji: user.emoji
+      })
     })
 
-    res.status(200).send(data);
+    res.status(200).send(data)
   })
 })
 
 app.post('/api/postPowerRankings', (req, res) => {
-  let currentTime = Moment.now();
+  let currentTime = Moment.now()
   let week = DateUtils.determineWeekOfSubmission(currentTime)
-
 
   let powerRankingData = {
     week: week,
     teamId: req.body.teamId,
-    rankings: req.body.rankings,
+    rankings: req.body.rankings
   }
 
-  let options = {upsert: true}
+  let options = { upsert: true }
 
   PowerRanking.updateOne(
     {
@@ -129,47 +135,56 @@ app.post('/api/postPowerRankings', (req, res) => {
     },
     {
       $set: {
-        rankings: [
-          req.body.rankings
-        ]
+        rankings: [req.body.rankings]
       }
     },
     options,
     (err, data) => {
-      if(err) {
+      if (err) {
         res.status(500).send('Something went wrong', err)
-      } else { // TODO - this is a bit of a lie. Doesn't guarantee rankings were updated.
+      } else {
+        // TODO - this is a bit of a lie. Doesn't guarantee rankings were updated.
         res.status(200).send('Successfully updated rankings')
       }
     }
   )
 })
 
+app.get('/api/getPowerRankings', (req, res) => {
+  let currentTime = Moment.now()
+  let week = DateUtils.determineWeekOfSubmission(currentTime)
+
+  let query = { weekId: week }
+  let data
+
+  PowerRanking.find(query, (err, rankings) => {
+    console.log('rankings', rankings)
+    data = rankings
+    res.status(200).send(data)
+  })
+})
+
 // TODO - Delete
 app.post('/powerRankings/makeUser', (req, res) => {
-  if(req.body.username &&
-     req.body.password &&
-     req.body.passwordConf) {
+  if (req.body.username && req.body.password && req.body.passwordConf) {
     var userData = {
       username: req.body.username,
       password: req.body.password,
-      passwordConf: req.body.passwordConf,
+      passwordConf: req.body.passwordConf
     }
 
     // use scheme.create to insert data into the db
     User.create(userData, function(err, user) {
-      if(err) {
+      if (err) {
         return next(err)
       } else {
-        return res.redirect('/profile');
+        return res.redirect('/profile')
       }
     })
   }
 })
 
-
 const port = 1337
 app.listen(port, () => {
-    console.log('App listening on port '+port+'.')
+  console.log('App listening on port ' + port + '.')
 })
-
