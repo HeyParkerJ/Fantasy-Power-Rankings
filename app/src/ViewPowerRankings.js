d1d0929813b013a564ab7df561bf5eb5e8191f1e
Parker Johnson <mrstormthebeach@gmail.com>
2018-11-01 07:25:56 -0700

    split out week by week selection and weekly display

--- app/src/ViewPowerRankings.js
+++ app/src/ViewPowerRankings.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import Requests from './http/requests'
+import PowerRankingWeekSelectionComponent from './PowerRankingWeekSelectionComponent'
 import PowerRankingComponent from './PowerRankingComponent'
 
 class ViewPowerRankings extends Component {
   constructor() {
     super()
     this.state = {
-      rankingsList: null
+      rankingsList: null,
+      selectedWeek: null,
+      // TODO - make latest
     }
   }
 
@@ -16,21 +19,41 @@ class ViewPowerRankings extends Component {
     })
   }
 
-  renderRankings = () => {
-    let rankingsDiv = []
+  setWeekToDisplay = week => {
+    this.setState({ selectedWeek: week })
+  }
+
+  renderWeekSelection = () => {
+    let weeksWithSubmissions = []
+
+
     if(this.state.rankingsList) {
-      this.state.rankingsList.forEach((ranking) => {
-        rankingsDiv.push(<PowerRankingComponent key={ ranking.teamId } ranking={ranking}/>)
+      Object.keys(this.state.rankingsList).forEach((week,index) => {
+        weeksWithSubmissions.push(week);
       })
     } else {
-      rankingsDiv.push(<p>Imagine these are the power rankings</p>)
+      return <p>Placeholder until we get the weeks back</p>
     }
-    return rankingsDiv
+
+    return <PowerRankingWeekSelectionComponent weeks={weeksWithSubmissions} handleClick={this.setWeekToDisplay}/>
   }
+
+  renderSelectedWeek = () => {
+    let rankings = []
+
+    if(this.state.selectedWeek && this.state.rankingsList) {
+      this.state.rankingsList[this.state.selectedWeek].forEach((ranking) => {
+        rankings.push(<PowerRankingComponent key={ranking.teamId} ranking={ranking} />)
+      })
+    }
+    return rankings
+  }
+
   render() {
     return (
       <div>
-        {this.renderRankings()}
+        {this.renderWeekSelection()}
+        {this.renderSelectedWeek()}
       </div>
     )
   }
