9ac8538e66d4f78367c97c6a063c51c975dcb2ab
Parker Johnson <mrstormthebeach@gmail.com>
2018-11-30 20:18:18 -0800

    [WIP-RVT] About to begin new attempt at algo impl

--- app/src/utils/RankingsUtils.js
+++ app/src/utils/RankingsUtils.js
@@ -42,8 +42,55 @@ export default {
     return placementsArray
   },
 
+  /* This is the sliding door algorithm.
+
+    Set a leader and an anchor index.
+    Start both at 0
+    Increment the leader index and compare it to the follower index
+    While
+      they match
+        Increment the leader index and check again
+      else
+        Set the dataPoint on every index between the two (make a walker index?)
+        Increment the follower index
+    */
+  // TODO - do real testing on this
+  appendTieAdjustedRankings: (placementsArray) => {
+    
+
+
+
+    placementsArray.forEach((placment, index, originalArray) => {
+      // Check next index for same average
+      let nextItemRankingsAverage = null
+      let nextItemRankingsAverageMatches = true
+      while(nextItemRankingsAverageMatches) {
+        if()
+      }
+      let nextItem = originalArray[index+1]
+
+      if (!nextItem) { return null }
+
+      if(nextItem.rankingsAverage === placement.rankingsAverage) {
+        // Keep looking at the next item until it doesn't match anymore
+        // If you find a match, add it to an array of matches
+        let loopingIndex = index
+        let nextItem = originalArray[index+loopingIndex]
+        while (nextItem) {
+          if(nextItem)
+          nextItem = 
+            loopingIndex++
+        }
+      }
+
+      debugger
+    })
+  },
+
+  /*
+    Used because emojis are a part of the users array, but not the rankingsArray object
+*/
   appendEmojisToRankingsArray: (rankingsArray, users) => {
-    debugger
     rankingsArray.forEach((r, index) => {
       users.forEach((u) => {
         if(u.teamId === r.teamId) {
