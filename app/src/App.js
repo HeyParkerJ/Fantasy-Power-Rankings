35898201050b915612e3ef2632aa3e149054c874
Parker Johnson <mrstormthebeach@gmail.com>
2019-09-29 15:12:24 -0600

    remove top level tab selector as we will only display powerrankings for now

--- app/src/App.js
+++ app/src/App.js
@@ -9,44 +9,16 @@ import Tab from '@material-ui/core/Tab'
 
 import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom'
 
-class TabsContainer extends Component {
-  state = {
-    selectedTab: 0
-  }
-
-  handleTabClick = (event, value) => {
-    this.setState({ selectedTab: value })
-  }
-
-  render() {
-    return (
-      <Tabs
-        centered
-        value={this.state.selectedTab}
-        onChange={this.handleTabClick}
-      >
-        <Tab label="Power Rankings" component={Link} to="/powerRankings" />
-        <Tab label="Raw Data" component={Link} to="/raw" />
-      </Tabs>
-    )
-  }
-}
-
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <AppBar position="static" color="default">
-            <TabsContainer />
-          </AppBar>
-
           <Route
             exact
             path="/"
             render={() => <Redirect to="/powerRankings/view" />}
           />
-          <Route path="/raw" component={RawData} />
           <Route path="/powerRankings" component={PowerRankings} />
         </div>
       </BrowserRouter>
